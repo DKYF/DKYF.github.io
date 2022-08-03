211b57dc81b12da7b27f944d150b66d36f748dca
DKYF <109020576+DKYF@users.noreply.github.com>
2022-08-03 21:33:48 +0800

    update

--- script.js
+++ script.js
@@ -11,29 +11,29 @@ btnNavEl.addEventListener("click", function () {
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
-const Introduction = document.querySelector(".introduction");
+// const Introduction = document.querySelector(".introduction");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
 
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
 
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(Introduction);
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(Introduction);
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
