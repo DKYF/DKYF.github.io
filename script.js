d65ecb4a0a4e9fcafed670080f6c072b67813e16
DKYF <109020576+DKYF@users.noreply.github.com>
2022-08-08 20:10:36 +0800

    update

--- script.js
+++ script.js
@@ -63,3 +63,31 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+// SLIDER
+
+let slides = document.getElementsByClassName("slider__slide");
+let currentSlide = 0;
+
+document.getElementById("nav-button--next").addEventListener("click", () => {
+  changeSlide(currentSlide + 1);
+});
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+  changeSlide(currentSlide - 1);
+});
+
+function changeSlide(moveTo) {
+  if (moveTo >= slides.length) {
+    moveTo = 0;
+  }
+  if (moveTo < 0) {
+    moveTo = slides.length - 1;
+  }
+
+  slides[currentSlide].classList.toggle("active");
+  slides[moveTo].classList.toggle("active");
+
+  currentSlide = moveTo;
+}
+
+// SLIDES
