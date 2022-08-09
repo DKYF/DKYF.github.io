03c42fd24c8828d5b22bf6ae555c7d7cc6cc87b6
DKYF <109020576+DKYF@users.noreply.github.com>
2022-08-09 16:57:43 +0800

    update

--- uMeat/script.js
+++ uMeat/script.js
@@ -1,4 +1,4 @@
-// SLIDER FOR DEALS
+// SLIDER FOR DEALS (NORMAL)
 
 let slides = document.getElementsByClassName("deals-info");
 let currentSlide = 0;
@@ -24,7 +24,33 @@ function changeSlide(moveTo) {
   currentSlide = moveTo;
 }
 
-// SLIDER FOR DEALS
+// SLIDER FOR DEALS (PHONE VIEW)
+
+let smallslides = document.getElementsByClassName("deals-info_2");
+let currentsmallSlide = 0;
+
+document.getElementById("nav-button--next_2").addEventListener("click", () => {
+  changesmallSlide(currentsmallSlide + 1);
+});
+document.getElementById("nav-button--prev_2").addEventListener("click", () => {
+  changesmallSlide(currentsmallSlide - 1);
+});
+
+function changesmallSlide(moveTo) {
+  if (moveTo >= smallslides.length) {
+    moveTo = 0;
+  }
+  if (moveTo < 0) {
+    moveTo = smallslides.length - 1;
+  }
+
+  smallslides[currentsmallSlide].classList.toggle("active");
+  smallslides[moveTo].classList.toggle("active");
+
+  currentsmallSlide = moveTo;
+}
+
+// SLIDER FOR CUSTOMER
 
 let customer = document.getElementsByClassName("testimonials-info");
 let currentcustomer = 0;
@@ -49,3 +75,40 @@ function changeCustomer(moveTo) {
 
   currentcustomer = moveTo;
 }
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
