13f41a5a80027b3ceab2ebe20aa0bd0223cff51f
DKYF <109020576+DKYF@users.noreply.github.com>
2022-08-18 21:28:25 +0800

    update

--- Brag/GrubFood/script.js
+++ Brag/GrubFood/script.js
@@ -6,3 +6,19 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// FAQ drop down
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
